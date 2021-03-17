2a668918788a6e425b9f3c6cfe7847f10d79709a
Linnea <ljc42000@gmail.com>
2021-03-17 10:52:04 -0400

    Added calls of the class and function to get audio files and voice list

--- SpeakCommands.py
+++ SpeakCommands.py
@@ -1,12 +1,15 @@
 #Code to take text and implement the Google Text-to-Speech API to create audio files for the commands
-import os
 
+#Import os and texttospeech from the Google cloud
+import os
 from google.cloud import texttospeech
 
-global json
+#Set json key as a global variable and set value
+global json_file
 
 json_file = "GCPKey.json"
 
+#Class to take text and synthesize a voice
 class Speak():
     def __init__(self, text, filename):
         self.text = text
@@ -22,7 +25,7 @@ class Speak():
         synthesis_input = texttospeech.SynthesisInput(text=self.text)
 
         # Build the voice request, select the language code ("en-US") and the ssml
-        # voice gender ("neutral")
+        # voice gender ("neutral") [THIS CAN BE CHANGED]
         voice = texttospeech.VoiceSelectionParams(
             language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
         )
@@ -43,3 +46,42 @@ class Speak():
             # Write the response to the output file.
             out.write(response.audio_content)
             print('Audio content written')
+
+#Function to list all available voices
+def list_voices():
+    client = texttospeech.TextToSpeechClient()
+
+    # Performs the list voices request
+    voices = client.list_voices()
+
+    #For all the voices avaiable print the name, language, gender, and hertz rate
+    for voice in voices.voices:
+        # Display the voice's name. Example: tpc-vocoded
+        print(f"Name: {voice.name}")
+
+        # Display the supported language codes for this voice. Example: "en-US"
+        for language_code in voice.language_codes:
+            print(f"Supported language: {language_code}")
+
+        ssml_gender = texttospeech.SsmlVoiceGender(voice.ssml_gender)
+
+        # Display the SSML Voice Gender
+        print(f"SSML Voice Gender: {ssml_gender.name}")
+
+        # Display the natural sample rate hertz for this voice. Example: 24000
+        print(f"Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}\n")
+
+#Call to get list of all the voices available, simply uncomment this and comment out call to the Speak class
+# list_voices()
+
+#Command to say key word
+keyWordCommand = Speak("Say a key word", "key_word_command.wav")
+keyWordCommand.talk()
+
+#Command to set key word
+keyWordSet = Speak("Say a key word to set it", "key_word_Set.wav")
+keyWordSet.talk()
+
+#Command to set story title
+storyNameSet = Speak("Say the name of the story", "story_name_set.wav")
+storyNameSet.talk()
