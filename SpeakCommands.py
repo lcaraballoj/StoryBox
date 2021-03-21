8f1479d042f6750a05a2aa79a065a5d9d42a1bda
Linnea <ljc42000@gmail.com>
2021-03-21 17:43:04 -0400

    Can change voice in SpeakCommands.py and added *.mp3 to .gitignore

--- SpeakCommands.py
+++ SpeakCommands.py
@@ -9,6 +9,8 @@ global json_file
 
 json_file = "GCPKey.json"
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_file
+
 #Class to take text and synthesize a voice
 class Speak():
     def __init__(self, text, filename):
@@ -16,7 +18,7 @@ class Speak():
         self.filename = filename
 
     def talk(self):
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_file
+        # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_file
 
         # Instantiates a client
         client = texttospeech.TextToSpeechClient()
@@ -25,9 +27,12 @@ class Speak():
         synthesis_input = texttospeech.SynthesisInput(text=self.text)
 
         # Build the voice request, select the language code ("en-US") and the ssml
+        #Name can be changed
         # voice gender ("neutral") [THIS CAN BE CHANGED]
         voice = texttospeech.VoiceSelectionParams(
-            language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+            language_code="en-US",
+            name = "en-US-Wavenet-H",
+            ssml_gender = texttospeech.SsmlVoiceGender.FEMALE
         )
 
         # Select the type of audio file you want returned
@@ -47,14 +52,15 @@ class Speak():
             out.write(response.audio_content)
             print('Audio content written')
 
-#Function to list all available voices
 def list_voices():
+    #Lists the available voices.
+    from google.cloud import texttospeech
+
     client = texttospeech.TextToSpeechClient()
 
     # Performs the list voices request
     voices = client.list_voices()
 
-    #For all the voices avaiable print the name, language, gender, and hertz rate
     for voice in voices.voices:
         # Display the voice's name. Example: tpc-vocoded
         print(f"Name: {voice.name}")
@@ -71,17 +77,18 @@ def list_voices():
         # Display the natural sample rate hertz for this voice. Example: 24000
         print(f"Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}\n")
 
+
 #Call to get list of all the voices available, simply uncomment this and comment out call to the Speak class
-# list_voices()
+#list_voices()
 
 #Command to say key word
-keyWordCommand = Speak("Say a key word", "key_word_command.wav")
+keyWordCommand = Speak("Say a key word", "key_word_command.mp3")
 keyWordCommand.talk()
 
 #Command to set key word
-keyWordSet = Speak("Say a key word to set it", "key_word_Set.wav")
+keyWordSet = Speak("Say a key word to set it", "key_word_Set.mp3")
 keyWordSet.talk()
 
 #Command to set story title
-storyNameSet = Speak("Say the name of the story", "story_name_set.wav")
+storyNameSet = Speak("Say the name of the story", "story_name_set.mp3")
 storyNameSet.talk()
