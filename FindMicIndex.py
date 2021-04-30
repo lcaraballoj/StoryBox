7f9ea20806ef3006f4581be62061a597f2f61a89
Linnea <ljc42000@gmail.com>
2021-04-30 13:51:26 -0400

    Debugged

--- FindMicIndex.py
+++ FindMicIndex.py
@@ -2,8 +2,8 @@ import pyaudio
 
 p = pyaudio.PyAudio()
 info = p.get_host_api_info_by_index(0)
-numdevices = infor.get('deviceCount')
+numdevices = info.get('deviceCount')
 
 for i in range(0, numdevices):
-    if (p.getget_device_info_by_host_api_device_index(host_api_index(0,i).get('maxInputChannels')) > 0:
+    if (p.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')) > 0:
         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name'))
